9a17cd94096962863006f2c494d34db72a2b8e30
Nicholas Schwab <git@nicholas-schwab.de>
2021-08-02 20:13:27 +0200

    Use pathlib instead of os.path.

--- src/loader/dataset_loader.py
+++ src/loader/dataset_loader.py
@@ -1,5 +1,4 @@
-import os
-from os.path import join, exists
+from pathlib import Path
 
 from datetime import datetime
 import networkx as nx
@@ -17,23 +16,27 @@ def load_dataset(dataset_name):
         graph_nx: networkx - graph of the dataset with all needed attributes
         folder_path: str - the path to the dataset folder, in order to dump files in it
     '''
-    folder_path = join(r"../data", dataset_name)
-    dump_folder = join(folder_path, 'dump')
+    folder_path = Path("../data") / dataset_name
+    dump_folder = folder_path / "dump"
 
     if dataset_name == 'PPI':
-        graph_path = join(folder_path, "interactions_with_dates.csv")
-        graph_df = pd.read_csv(graph_path, sep=',', usecols=['id1', 'id2', 'discovery date'])
+        graph_path = folder_path / "interactions_with_dates.csv"
+        graph_df = pd.read_csv(graph_path, sep=',', usecols=[
+                               'id1', 'id2', 'discovery date'])
         graph_df = graph_df[~pd.isnull(graph_df['discovery date'])]
-        graph_df['time'] = graph_df['discovery date'].map(lambda x: eval(x).year)
-        graph_df.rename({'id1': 'from', 'id2': 'to'}, axis='columns', inplace=True)
-        graph_nx = nx.from_pandas_edgelist(graph_df, 'from', 'to', edge_attr=['time'], create_using=nx.Graph())
+        graph_df['time'] = graph_df['discovery date'].map(
+            lambda x: eval(x).year)
+        graph_df.rename({'id1': 'from', 'id2': 'to'},
+                        axis='columns', inplace=True)
+        graph_nx = nx.from_pandas_edgelist(graph_df, 'from', 'to', edge_attr=[
+                                           'time'], create_using=nx.Graph())
     else:
         raise Exception('dataset not available')
 
-    if not exists(dump_folder):
-        os.mkdir(dump_folder)
+    dump_folder.mkdir(exists_ok=True)
 
     return graph_nx, dump_folder
+
 
 def df2graph(graph_df, source, target, time, create_using=nx.Graph()):
     return nx.from_pandas_edgelist(graph_df, source, target, edge_attr=[time], create_using=create_using)
